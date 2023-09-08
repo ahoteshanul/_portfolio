6bf1c3ab2bae4b4333231081fe7bb0ec142da5c3
ahoteshanul <ahoteshanul@gmail.com>
2023-09-08 19:49:27 +0600

    Location Changed.

---
+++ cypress/e2e/WI Platform/Wi-bazaar-admin/WIbazaar.cy.js
@@ -0,0 +1,6 @@
+// context('Actions', () => {
+//     beforeEach(() => {
+//       cy.visit('http://104.248.152.206/')
+//     })})
+import webpack from "@cypress/webpack-preprocessor";
+    cy.visit('http://104.248.152.206/');
