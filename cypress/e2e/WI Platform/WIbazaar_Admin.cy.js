10b000bf9fddf12dceb30f3bcf1085919fe5b658
ahoteshanul <ahoteshanul@gmail.com>
2023-09-09 21:57:19 +0600

    Update script

--- cypress/e2e/WI Platform/WIbazaar_Admin.cy.js
+++
@@ -1,6 +0,0 @@
-describe("Wibazaar Admin", () => {
-    it("Sign in as Wibazaar Admin", () => {
-      cy.visit("http://104.248.152.206")
-    })
-  })
-  
---
+++ cypress/e2e/WI Platform/Wiaccccount.cy.js
@@ -0,0 +1,15 @@
+import {faker} from '@faker-js/faker';
+
+describe("Wiaccount", () => {
+    it("Sign up in Wiaccount", () => {
+
+      const email = faker.internet.email();
+
+      cy.visit("http://159.89.205.48/") // Wiaccount 
+      cy.get(':nth-child(4) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
+      cy.get(':nth-child(5) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
+      cy.get(':nth-child(6) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
+      cy.get(':nth-child(7) > .inputGroupContainer > .input-group > .form-control').type(email)
+    })
+  })
+  
