49b83722227fa337b8285728a6e8fe2dbb776f3a
ahoteshanul <ahoteshanul@gmail.com>
2023-09-10 22:40:03 +0600

    Update script

--- cypress/e2e/WI Platform/Wiaccccount.cy.js
+++ cypress/e2e/WI Platform/Wiaccccount.cy.js
@@ -5,7 +5,8 @@ describe("Wiaccount", () => {
 
       const email = faker.internet.email();
 
-      cy.visit("http://159.89.205.48/") // Wiaccount 
+      cy.visit("http://159.89.205.48/") // Wiaccount
+      cy.get(':nth-child(2) > .inputGroupContainer > .input-group > .form-control').attachFile(filepath)
       cy.get(':nth-child(4) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
       cy.get(':nth-child(5) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
       cy.get(':nth-child(6) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
