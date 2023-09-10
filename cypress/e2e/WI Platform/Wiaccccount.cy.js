02a5a822e0a12f81d14a69b8c8d6db5f3f530fca
ahoteshanul <ahoteshanul@gmail.com>
2023-09-10 22:51:44 +0600

    Added images and upload image mechanism

--- cypress/e2e/WI Platform/Wiaccccount.cy.js
+++ cypress/e2e/WI Platform/Wiaccccount.cy.js
@@ -6,11 +6,12 @@ describe("Wiaccount", () => {
       const email = faker.internet.email();
 
       cy.visit("http://159.89.205.48/") // Wiaccount
-      cy.get(':nth-child(2) > .inputGroupContainer > .input-group > .form-control').attachFile(filepath)
+      cy.get(':nth-child(2) > .inputGroupContainer > .input-group > .form-control').selectFile('/home/professor/Desktop/Automation-Sandwich/cypress/images/cartoon-character-with-yellow-jacket-sunglasses.jpg')
+      cy.get(':nth-child(3) > .inputGroupContainer > .input-group > .form-control').selectFile('/home/professor/Desktop/Automation-Sandwich/cypress/images/cartoon-character-with-yellow-jacket-sunglasses.jpg')
       cy.get(':nth-child(4) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
       cy.get(':nth-child(5) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
       cy.get(':nth-child(6) > .inputGroupContainer > .input-group > .form-control').type('Hello, World')
       cy.get(':nth-child(7) > .inputGroupContainer > .input-group > .form-control').type(email)
     })
   })
-  
+  cy.get(':nth-child(3) > .inputGroupContainer > .input-group > .form-control')
